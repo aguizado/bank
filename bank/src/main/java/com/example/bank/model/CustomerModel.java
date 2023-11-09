48416a2698e9d3ff1d4489fe0bf5f163775db535
Andres de Felipe Guizado Diaz <aguizado@emeal.nttdata.com>
2023-11-09 09:20:35 -0500

    Avance 0911 0920

--- bank/src/main/java/com/example/bank/model/CustomerModel.java
+++ bank/src/main/java/com/example/bank/model/CustomerModel.java
@@ -1,6 +1,8 @@
 package com.example.bank.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -33,5 +35,63 @@ public class CustomerModel {
 
   @JsonProperty("type_customer")
   private CustomerTypeModel typeCustomer;
+  
+  /**
+   * . Enum DescriptionEnum
+   *
+   * @author Andres Guizado
+   * @version 0.1, 2023/10/27
+   */
+  public enum TypeDocumentEnum {
+    
+    DNI("DNI"), CEX("CEX"), PASSPORT("passport");
+
+    private String value;
+
+    TypeDocumentEnum(String value) {
+      this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+      return value;
+    }
+
+    @Override
+    public String toString() {
+      return String.valueOf(value);
+    }
+
+    /**
+     * . This method is fromValue DescriptionEnum
+     *
+     * @param value This is the first parameter
+     * @return a new value
+     */
+    @JsonCreator
+    public static TypeDocumentEnum fromValue(String value) {
+      for (TypeDocumentEnum b : TypeDocumentEnum.values()) {
+        if (b.value.equals(value)) {
+          return b;
+        }
+      }
+      throw new IllegalArgumentException("Unexpected value '" + value + "'");
+    }
+  }
+
+  @JsonProperty("type_document")
+  private TypeDocumentEnum typeDocument = TypeDocumentEnum.DNI;
+
+  @JsonProperty("document_numer")
+  private String documentNumer;
+
+  @JsonProperty("phone")
+  private String phone;
+
+  @JsonProperty("imei")
+  private String imei;
+
+  @JsonProperty("email")
+  private String email;
 
 }
--- bank/src/main/java/com/example/bank/model/dto/CustomerDto.java
+++ bank/src/main/java/com/example/bank/model/dto/CustomerDto.java
@@ -1,6 +1,8 @@
 package com.example.bank.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -31,5 +33,63 @@ public class CustomerDto {
 
   @JsonProperty("type_customer")
   private CustomerTypeDto typeCustomer;
+  
+  /**
+   * . Enum DescriptionEnum
+   *
+   * @author Andres Guizado
+   * @version 0.1, 2023/10/27
+   */
+  public enum TypeDocumentEnum {
+    
+    DNI("DNI"), CEX("CEX"), PASSPORT("passport");
+
+    private String value;
+
+    TypeDocumentEnum(String value) {
+      this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+      return value;
+    }
+
+    @Override
+    public String toString() {
+      return String.valueOf(value);
+    }
+
+    /**
+     * . This method is fromValue DescriptionEnum
+     *
+     * @param value This is the first parameter
+     * @return a new value
+     */
+    @JsonCreator
+    public static TypeDocumentEnum fromValue(String value) {
+      for (TypeDocumentEnum b : TypeDocumentEnum.values()) {
+        if (b.value.equals(value)) {
+          return b;
+        }
+      }
+      throw new IllegalArgumentException("Unexpected value '" + value + "'");
+    }
+  }
+
+  @JsonProperty("type_document")
+  private TypeDocumentEnum typeDocument = TypeDocumentEnum.DNI;
+
+  @JsonProperty("document_numer")
+  private String documentNumer;
+
+  @JsonProperty("phone")
+  private String phone;
+
+  @JsonProperty("imei")
+  private String imei;
+
+  @JsonProperty("email")
+  private String email;
 
 }
